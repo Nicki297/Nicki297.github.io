3e077206c1802a358042cc3014748a838d714973
Nicki297 <61786858+Nicki297@users.noreply.github.com>
2023-01-10 11:43:55 +0100

    update

--- Entwicklungsaufgaben/Sternenhimmel/js/main.js
+++ Entwicklungsaufgaben/Sternenhimmel/js/main.js
@@ -4,15 +4,15 @@ let img_list = [
     "https://upload.wikimedia.org/wikipedia/commons/9/95/New_shot_of_Proxima_Centauri%2C_our_nearest_neighbour.jpg",
     "https://us.123rf.com/450wm/smilyk/smilyk1611/smilyk161100036/65602872-gl%C3%BChender-realistischer-blauer-stern-stilisiertes-objekt-kosmisches-konzept-.jpg",
     "https://qph.cf2.quoracdn.net/main-qimg-769c79fe317d0cc35d2413c578bb8211-lq"
-]
+];
 
-let star_list = []
+let star_list = [];
 
 function create_star() {
     const el = document.createElement("div");
     el.setAttribute("class", "star_img");
     // el.setAttribute("src", img_list[~~(Math.random()*img_list.length)]);
-    el.style.background = `black url(${img_list[~~(Math.random()*img_list.length)]}) center no-repeat`
+    el.style.background = `black url(${img_list[~~(Math.random()*img_list.length)]}) center no-repeat`;
     // el.style.backgroundImage = `url(${img_list[~~(Math.random()*img_list.length)]})`
     el.style.top = `${Math.random() * 100}vh`; 
     el.style.left = `${Math.random() * 100}vw`;
@@ -33,16 +33,16 @@ function create_star_rnd() {
         create_star();
         t = Math.random() * 3000;
         create_star_rnd();
-    },t)
+    },t);
 }
 
 function remove_rnd_star() {
     setInterval(() => {
         if (star_list.length>10) {
-            let i = ~~(Math.random()*star_list.length)
+            let i = ~~(Math.random()*star_list.length);
             star_list[i].style.opacity = 0;
         }
-    }, t2)  
+    }, t2);
 }
 
 function clear_star_list() {
